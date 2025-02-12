0457f44ef771a407d774440a9d1ef11dc0f95f9c
yuyuc03 <yuyucheng2003@gmail.com>
2025-02-12 09:08:47 +0000

    Done fetching the lastest data in the database, but need to change abit the database format

--- lib/machine_learning/database.py
+++ lib/machine_learning/database.py
@@ -2,6 +2,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import text
 
 SQLALCHEMY_DATABASE_URL = "sqlite:////Users/yuyu/Library/Developer/CoreSimulator/Devices/B5D0593B-15E9-4CBA-8109-7C7476E5F0BD/data/Containers/Data/Application/0E542441-182F-4935-AC39-32766E7E7B4C/Documents/health_metrics.db"  
 
@@ -10,9 +11,17 @@ engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
 
-# Test the connection
-with engine.connect() as connection:
-    print("Connection successful")
+def fetch_latest_data():
+    with engine.connect() as connection:
+        print ("Connection Successful") # For Debug Use
+        result = connection.execute(text("SELECT * FROM health_metrics ORDER BY timestamp DESC LIMIT 1"))
+        
+        # Fetch one row and convert it to a dictionary
+        row = result.mappings().first()
+        if row:
+            return dict(row)
+        else:
+            return None
 
 # Creating a database session object which can use to interact with database (such as querying, inserting, or updating data)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -25,3 +34,10 @@ def get_db():
     finally:
         db.close()
 
+#  Call fech_latest_data() to test the connection and fetch data
+if __name__ == "__main__":
+    try:
+        latest_data = fetch_latest_data()
+        print("Latest Data:", latest_data)
+    except Exception as e:
+        print(f"Error: {e}")
